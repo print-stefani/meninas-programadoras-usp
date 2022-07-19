eea3f9b175f28f3bcdb5c8fa21b9596ead73bf13
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 11:18:26 -0300

    Create epar.py
    Introdução
    Leia um valor inteiro e verifique se ele é par .

---
+++ epar.py
@@ -0,0 +1,10 @@
+#Programa que verifica se o numero digitado é par ou não, usando estrutura de condição
+
+#leitura do dado
+numero  = int (input("Digite um numero inteiro: "))
+
+#verificação das condições
+if numero % 2 == 0:
+  print (numro, "é par")
+else:
+  print (numero,"não é par")
