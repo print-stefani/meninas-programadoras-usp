5deea007773c8b6b9e309057897209704e6349c6
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 10:43:27 -0300

    Create atributos-animais.py
    Introdução
    Animais do mundo real como patos, gatos, abelhas e lesmas têm alguns atributos (ou características) em comum e outros atributos que os distinguem. Por exemplo, considerando o número de pés (ou patas) eles apresentam, respectivamente, 2, 4, 6 e 0 pés. Outros atributos são mais específicos: em termos de camada externa, esses animais apresentam, por exemplo, penas, pelos, asas ou caracol. Assim, podemos dizer que cada objeto do tipo animal tem uma série de atributos.

---
+++ atributos-animais.py
@@ -0,0 +1,8 @@
+#Programa que lê as informações dadas pelo usuario e imprime sobre um animal, como nos exemplos: nome, ação, caracteristica 1, caracteristica 2
+
+nome = input ("Digite o nome do animal: ")
+atributo_1 = input ("Digite uma caracteristica do animal: ")
+atributo_2 = input ("Digite uma segunda caracteristica do animal: ")
+atividade = input ("Digite uma ação do animal: ")
+
+print (nome, atividade, atributo_1, atributo_2)
