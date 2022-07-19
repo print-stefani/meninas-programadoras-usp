f05472bbc965345fd4e3ca711e2062f967a6e870
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 12:30:50 -0300

    Create leitura-de-lista.py
    Introdução
    Seu programa pode ter que ler vários dados de um mesmo tipo em uma única linha, deve imprimir, na forma de uma lista, os dados fornecidos como entrada. Na primeira linha, uma entre as opções s (caractere), f (inteiros) e n (decinal).

---
+++ week 2/leitura-de-lista.py
@@ -0,0 +1,20 @@
+#Programa que le dados do mesmo tipo na mesma linha usando o split(), retornando uma lista
+
+#informar possiveis listas nesse programa
+print(" f - valores inteiros \n s - sequencia de caracteres \n n - valores decimais\n") 
+
+#leitura da entrada
+entrada = input ("Digite a opção escolhida: ")
+
+#verificação da entrada para formatação do split
+if entrada == 'f':
+  nota_final = [int (i) for i in input().split()]
+  print (nota_final)
+
+elif entrada == 'n':
+  nota = [float(i) for i in input().split()]
+  print (nota)
+
+elif entrada == 's':
+  sobrenome = [str(i) for i in input().split()]
+  print (sobrenome)
