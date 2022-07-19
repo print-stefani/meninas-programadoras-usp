a4f265426f068e062a46decfeb803401d48564a9
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 13:25:16 -0300

    contagem de linhas do poema
    Seu programa deve ler um número indeterminado de strings, e imprimir o número de strings lidas como saída desde que a string não seja nula, ou seja, vazia. O programa deve terminar quando a string CDA 1942 for lida.

---
+++ week 2/poema-carlos-drummond.py
@@ -0,0 +1,18 @@
+#Programa que le dados indeterminados de linhas, e de saida imprime o numero de linhas lidas, menos os vazios.
+#termina quando for digitado CDA 1942 - usando a estrutura de repetição while
+
+#instrução
+linha = input ("Digite o titulo e o poema de Carlos Drummond:\n")
+
+#define variavel inicial
+texto = 0
+
+#estrutura de repetição
+while linha != 'CDA 1942':
+  
+  #verificação da condição
+  if linha != '':
+    texto = texto + 1
+  linha = input ()
+
+print (texto)
