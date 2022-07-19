c293d901df8f41812e31cd94bef4c3069224ac45
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 11:04:17 -0300

    Create rejuvenescimento-dois-anos.py
    Introdução
    Seu programa deve ler dois valores, nome e idade, e imprimi-los na ordem inversa, diminuindo a idade em dois anos :-)

---
+++ rejuvenescimento-dois-anos.py
@@ -0,0 +1,12 @@
+#Programa que lê dois valores, sendo uma string (nome) e um inteiro (idade), devolvendo de saida a idade reduzida em dois anos
+
+#leitura das informações
+nome = input ("Digite seu nome: ")
+idade = int (input ("Digite sua idade: "))
+
+#calculo do rejuvenescimento
+rejuvenescimento = idade - 2
+
+#saida do programa
+print (rejuvenescimento)
+print (nome)
