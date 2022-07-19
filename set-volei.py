649b06c2ba4841fe1f2ee51f89a12c638ddd7237
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 10:34:58 -0300

    Update set-volei.py
    Um jogo de vôlei é disputado em uma sequência de sets. Um set normalmente termina em 25 pontos mas. Entretanto, em caso de empate em 24 pontos, o set só terminará quando uma equipe alcançar uma diferença de 2 pontos em relação à outra equipe: nesse caso, não há ponto limite para o término do set.

--- set-volei.py
+++ set-volei.py
@@ -1,2 +1,15 @@
 #Programa que recebe a pontuação de dois times ao final de um set verificando se acabou a partida
 
+#leitura das informações do usuario
+time_1  =  int ( input ( "Digite os pontos do tempo 1: " ))
+time_2  =  int ( input ( "Digite os pontos do tempo 2: " ))
+
+#calculo da diferença de pontos entre os times
+diferenca  =  time_1  -  time_2
+diferenca_2  =  hora_2  -  hora_1
+
+#condição de verificação se o set acaba ou não
+if ( time_1  ==  25  ou  time_2  ==  25 ) e ( diferenca  >  2  ou  diferenca_2  >  2 ):
+  print ( 'Sim, o set acabou' )
+mais :
+  print ( 'Não, o set continua' )
