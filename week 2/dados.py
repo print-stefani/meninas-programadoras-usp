52ce11da32396c661742d2ca8b884b073fa4605e
Stefani  Beatriz Carvalho Vasconcellos <105372676+print-stefani@users.noreply.github.com>
2022-07-19 12:46:45 -0300

    Create dados.py
    Introdução
    Um dado é um dispositivo muito útil para ensinar crianças a aprenderem a contar. Pensando nisso, você teve a ideia de que um programa que conta os pontos de um dados pode ser útil! Para começar, você decidiu fazer o programa para contar os  um pontos de dado de 6 faces. Cada face tem entre 0 e 6 pontos distribuídos em 9 posições organizadas em três linhas x três colunas. Seu programa considera pontos com valor 1 e posições vazias com valor 0.

---
+++ week 2/dados.py
@@ -0,0 +1,24 @@
+#Programa que conta os pontos de dado de 6 faces, distribuido em 9 posições (3 linhas e 3 colunas)
+
+#instrução
+print ("Digite 0 - sem ponto ou 1 - ponto, em cada linha")
+
+#leitura das posições
+linha_1 = [int (i) for i in input().split()]
+linha_2 = [int (i) for i in input().split()]
+linha_3 = [int (i) for i in input().split()]
+
+#comprimento total da face 
+total = len (linha_1 or linha_2 or linha_3)
+
+#estrutura de repetição, i = inicio, analise do total
+for i in range (total):
+  
+  #contar todos 1 em cada face
+  total_1 = linha_1 .count (1)
+  total_2 = linha_2 .count (1)
+  total_3 = linha_3 .count (1)
+  
+  #soma o total em toda a face
+  soma = total_1 + total_2 + total_3
+print (soma)
